2f9f273559a038cf2ff994d25bebe58633c7021a
danicyber48 <37134596+danicyber48@users.noreply.github.com>
2018-09-08 16:38:39 +0700

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from .config import Config
+from .client import XL
+
+__copyright__       = 'Copyright 2018 By Danicyber48'
+__version__         = '3.0.0'
+__author__          = 'Dani☠Cyber48'
+__author_email__    = 'cristiandani66@gmail.com'
+__url__             = 'http://github.com/danicyber48/tembak-xl'
+
+__all__ = ['Config','XL']
